1a13cb2bc5a702f1b9deef2284d570c185b9d5f3
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-12-06 17:13:29 -0300

    Estudando useEffect

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
+
+  const [valor1, setValor1] = useState(0)
+
   return (
     <div className="UseRef">
       <PageTitle
         title="Hook UseRef"
         subtitle="Retorna um objeto mutável com a propriedade .current!"
       />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <input type="text" className="input" value={valor1} />
+      </div>
     </div>
   );
 };
