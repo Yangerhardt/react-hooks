3737dae62e358641fc8c2abe29e05789093edecd
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-12-07 16:21:54 -0300

    Exemplo de useMemo

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 
 const UseCallback = (props) => {
+  const [count, setCount] = useState(0);
+
+  function inc(delta) {
+    setCount
+  }
+
   return (
     <div className="UseCallback">
       <PageTitle
         title="Hook UseCallback"
         subtitle="Retorna uma função memoizada!"
       />
+
+      <div className="center">
+        <span className="text">{count}</span>
+
+        <div>
+          <button className="btn">+6</button>
+          <button className="btn">+12</button>
+          <button className="btn">+18</button>
+        </div>
+      </div>
     </div>
   );
 };
