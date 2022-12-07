0dc6fb6b729f46e6fcc673d0df35a3bc08781e61
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-12-07 15:30:37 -0300

    Exemplo do useRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
+  const [valor1, setValor1] = useState("");
+  const [valor2, setValor2] = useState("");
 
-  const [valor1, setValor1] = useState(0)
+  const count = useRef(0);
+  const meuInput1 = useRef(null);
+  const meuInput2 = useRef(null);
+
+  useEffect(() => {
+    count.current++;
+    meuInput2.current.focus()
+  }, [valor1]);
+
+  useEffect(() => {
+    count.current++;
+    meuInput1.current.focus()
+  }, [valor2]);
 
   return (
     <div className="UseRef">
@@ -14,7 +28,30 @@ const UseRef = (props) => {
       />
       <SectionTitle title="Exercício #01" />
       <div className="center">
-        <input type="text" className="input" value={valor1} />
+        <div>
+          <span className="text">Valor: </span>
+          <span className="text">{valor1} [</span>
+          <span className="text red">{count.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          type="text"
+          className="input"
+          value={valor1}
+          onChange={(e) => setValor1(e.target.value)}
+          ref={meuInput1}
+        />
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          value={valor2}
+          onChange={(e) => setValor2(e.target.value)}
+          ref={meuInput2}
+        />
       </div>
     </div>
   );
